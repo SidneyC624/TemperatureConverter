e41847388489434f551477357d7f2175932074bb
SidneyC624 <sidney.0503c@gmail.com>
2023-05-16 21:07:15 +0800

    switched to box layout

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -5,6 +5,7 @@ import java.awt.*;
 public class TemperatureConverter implements ActionListener{
 	
 	JFrame frame;
+	JPanel panel;
 	JLabel cLabel;
 	JLabel fLabel;
 	JTextField cTextField;
@@ -15,6 +16,7 @@ public class TemperatureConverter implements ActionListener{
 	TemperatureConverter() {
 		
 		frame = new JFrame("Temperature Converter");
+		panel = new JPanel();
 		cLabel = new JLabel("Celsius");
 		fLabel = new JLabel("Fahrenheit");
 		cTextField = new JTextField();
@@ -28,48 +30,50 @@ public class TemperatureConverter implements ActionListener{
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
 		
-		cLabel.setBounds(0,0,200,25);
+		panel.setBounds(0,0,180,220);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		cLabel.setSize(180,25);
 		cLabel.setForeground(new Color(50,0,75));
 		cLabel.setOpaque(false);
 		cLabel.setHorizontalAlignment(JTextField.CENTER);
 		cLabel.setFont(new Font("MV Boli", Font.BOLD, 20));
 		
-		cTextField.setBounds(25,25,150,25);
 		cTextField.setBorder(BorderFactory.createBevelBorder(1));
 		cTextField.setFont(new Font("Ink Free", Font.PLAIN, 15));
 		
-		fLabel.setBounds(0,75,200,25);
 		fLabel.setForeground(new Color(50,0,75));
 		fLabel.setOpaque(false);
 		fLabel.setHorizontalAlignment(JTextField.CENTER);
 		fLabel.setFont(new Font("MV Boli", Font.BOLD, 20));
 		
-		fTextField.setBounds(25,100,150,25);
 		fTextField.setBorder(BorderFactory.createBevelBorder(1));
 		fTextField.setFont(new Font("Ink Free", Font.PLAIN, 15));
 		
-		fLabel.setBounds(0,75,200,25);
 		fLabel.setForeground(new Color(50,0,75));
 		fLabel.setOpaque(false);
 		fLabel.setHorizontalAlignment(JTextField.CENTER);
 		fLabel.setFont(new Font("MV Boli", Font.BOLD, 20));
 		
-		cTofButton.setBounds(25,160,150,30);
 		cTofButton.setFont(new Font("MV Boli", Font.PLAIN, 15));
 		cTofButton.setFocusable(false);
 		cTofButton.addActionListener(this);
 		
-		fTocButton.setBounds(25,200,150,30);
 		fTocButton.setFont(new Font("MV Boli", Font.PLAIN, 15));
 		fTocButton.setFocusable(false);
 		fTocButton.addActionListener(this);
 		
-		frame.add(cLabel);
-		frame.add(cTextField);
-		frame.add(fLabel);
-		frame.add(fTextField);
-		frame.add(cTofButton);
-		frame.add(fTocButton);
+		panel.add(cLabel);
+		panel.add(cTextField);
+		panel.add(Box.createVerticalStrut(10));
+		panel.add(fLabel);
+		panel.add(fTextField);
+		panel.add(Box.createVerticalStrut(10));
+		panel.add(cTofButton);
+		panel.add(Box.createVerticalStrut(10));
+		panel.add(fTocButton);
+		
+		frame.add(panel);
 		
 		frame.setVisible(true);
 		
